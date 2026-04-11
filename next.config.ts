a7cc3b87ee39889d5d96797e762fc23af4e818cf
Ali Asghar <iamaliasghar37@gmail.com>
2026-04-11 20:38:13 +0500

    fix: remove old prisma generated files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
+    outputFileTracingIncludes: {
+        "/**": ["./prisma/generated/prisma/**/*.node"],
+    },
 };
 
 export default nextConfig;
