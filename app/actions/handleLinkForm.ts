790cd8e9ada5dcf265d9fef8b433e9bc5b9c85b9
Ali Asghar <iamaliasghar37@gmail.com>
2026-07-21 15:26:43 +0500

    Fix: Update LLM to GPT OSS 120B

--- app/actions/handleLinkForm.ts
+++ app/actions/handleLinkForm.ts
@@ -49,7 +49,7 @@ const generateSummary = async (longUrl: string) => {
         const groq = new Groq({ apiKey: process.env.GROQ_API });
 
         const response = await groq.chat.completions.create({
-            model: "llama-3.1-8b-instant",
+            model: "openai/gpt-oss-120b",
             messages: [
                 {
                     role: "system",
